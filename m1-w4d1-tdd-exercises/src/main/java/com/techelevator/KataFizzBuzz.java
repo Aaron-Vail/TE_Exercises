565f8bb336b890e042efae870ec472b502309613
Aaron <aaron.vail@yahoo.com>
2017-09-18 20:39:55 -0400

    vail, aaron fizzBuzz complete

--- m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
+++ m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,21 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
-
+	
+	public static Object fizzBuzz(int n) {
+		
+		if (n == 0)
+			return "0";
+		
+		String element = "";
+		
+		if (n > 100 || n < 0)
+			element += "null";
+		if (n % 3 == 0)
+			element += "Fizz";
+		if (n % 5 == 0)
+			element += "Buzz";
+		
+		return element.isEmpty() ? String.valueOf(n) : element;
+	}
 }
--- m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,50 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
-
+	
+	KataFizzBuzz sut;
+	
+//	@Before
+//	public void setup () {
+//		int result = sut.add("");
+//		sut = new KataFizzBuzz();
+//		
+//	}
+	@Test
+    public void testZero() {
+        assertEquals(KataFizzBuzz.fizzBuzz(0), "0");
+	}
+	@Test
+    public void testOne() {
+        assertEquals(KataFizzBuzz.fizzBuzz(1), "1");
+	}
+	@Test
+	public void testMultipleOfThreeFizz() {
+		assertEquals(KataFizzBuzz.fizzBuzz(3), "Fizz");
+	}
+	@Test
+	public void testMultipleOfFiveBuzz() {
+		assertEquals(KataFizzBuzz.fizzBuzz(5), "Buzz");
+	}
+	@Test
+	public void testSix() {
+		assertEquals(KataFizzBuzz.fizzBuzz(6), "Fizz");
+	}
+	@Test
+	public void testTen() {
+		assertEquals(KataFizzBuzz.fizzBuzz(10), "Buzz");
+	}
+	@Test
+    public void testMultipleOfThreeAndFiveFizzBuzz() {
+        assertEquals(KataFizzBuzz.fizzBuzz(15), "FizzBuzz");
+	}
+	
+	@Test
+	public void testUpperLimit() {
+		assertEquals(KataFizzBuzz.fizzBuzz(101), "null");
+	}
+	@Test
+	public void testLowerLimit() {
+		assertEquals(KataFizzBuzz.fizzBuzz(-1), "null");
+	}
 }
