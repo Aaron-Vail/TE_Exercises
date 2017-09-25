ea0e6172bf2a0ac3ad9dec85d39c297716dea15d
Aaron <aaron.vail@yahoo.com>
2017-09-25 15:40:01 -0400

    w5d1 afternoon exercises complete

--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
@@ -1,10 +1,61 @@
-package com.techelevator;
-
-public class FileSplitter {
-
-	public static void main(String[] args) {
-		
-
-	}
-
-}
+//package com.techelevator;
+//
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.PrintWriter;
+//import java.util.Scanner;
+//
+//public class FileSplitter {
+//
+//	public static void main(String[] args) {
+//		
+//		String fileName = null;
+//		int maxLines = 0;
+//		
+//		File newDoc = new File(""); 
+//		File originalDoc = new File("");	
+//		
+//		
+//		try(Scanner userInput = new Scanner(System.in)) {
+//			
+//			System.out.println("What is the name of the text file to be split?");
+//			fileName = userInput.nextLine();
+//		
+//			System.out.println("What is the maximum number of lines in each output file?");
+//			maxLines = userInput.nextInt();
+//		}
+//		
+//		
+//		try(
+//				
+//			Scanner readDoc = new Scanner(originalDoc); 
+//			PrintWriter writeDoc = new PrintWriter(newDoc)  
+////					
+////		) {
+////			public boolean split(int size) {
+////				if (size < 0);
+////				return false;
+////		try(
+////			int parts = ((int) newDoc.length() / maxLines);	
+////			int fileLength = 0;
+////			if (newDoc.length() % size > 0)
+////				parts++;
+////			
+////			)
+////			}
+//			while(readDoc.hasNextLine()) {
+//				String i = null;
+//				String line = readDoc.nextLine(); 
+//				if(line.contains("")) {  
+//					line = line.concat(i); 
+//				}
+//				writeDoc.println(line);  
+//			}  
+//		} catch (FileNotFoundException e) {
+//			System.out.println("The pom.xml doesn't exist");
+//			System.exit(1);
+//		}
+//		
+//	}
+//
+//}
--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
@@ -1,10 +1,33 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
 		
-
+		File fizzWrite = new File("fizzBuzz.txt");
+		
+		try(PrintWriter writeFizz = new PrintWriter(fizzWrite)) {
+			
+		for (int i = 1; i <= 300; i++) {
+			if (i % 15 == 0) {
+				writeFizz.println("FizzBuzz");
+			 }    
+			 else if (i % 3 == 0 || Integer.toString(i).contains("3")) {
+				 writeFizz.println("Fizz");
+			 }     
+			 else if (i % 5 == 0 || Integer.toString(i).contains("5")) {
+				 writeFizz.println("Buzz");       
+			 }else {
+				writeFizz.println(Integer.toString(i));
+			 }
+			}	
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Cannot complete Fizz Buzz Writer!");
+		}
 	}
-
 }
